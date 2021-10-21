19520d03f753c0c2675978d7cfd6ed467f52848b
yologan129 <logan128128@gmail.com>
2021-10-21 13:57:51 -0500

    created new exercise

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Logan";
+            int age = 21;
+            char myCharacter = 'J';
+            bool isAlive = true;
+            decimal mydecimal = 7.95m;
+            double housePayment = 800;
+            Console.WriteLine($"Hello my name is {firstName} I am {age} my name doesnt start with {myCharacter} {isAlive} I pay a lot for our house we both pay {housePayment} ");
+
+        }
+    }
+}
